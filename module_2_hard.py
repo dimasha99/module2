c3d54df1165f531d1c14dc317dc4ce70de8e4bcb
dimasha99 <dimasha2604@mail.ru>
2024-08-31 20:23:48 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,23 +1,9 @@
-def generate_password(n):
-    result = ""
-    for i in range(1, n):
-        for j in range(i+1, n):
-            if (i + j) % n == 0:
-                result += str(i) + str(j)
-    return result
-from random import randint
+while True:
+    number = int(input('Введите число от 3 до 20: '))
+    result = ''
 
-number = 0
-while number < 3 or number > 20:
-    number = int(input("Введите число для пароля от 3 до 20: "))
-
-password = generate_password(number)
-print(f"Пароль для числа {number}: {password}")
-print("Поздравляю,вы сбежали")
-n= randint(3,20)
-result = ""
-for i in range(1, n):
-    for j in range(i+1, n):
-        if n % (i + j) == 0 :
-            result += f'{i}{j}'
-print(f'{n} - {result}')
+    for i in range(1, number):
+        for j in range(i+1, number):
+            if number % (i+j) == 0:
+                result += f'{i}{j}'
+    print(f'{number} - {result}')
